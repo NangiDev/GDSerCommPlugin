8ddd18aa3199be2deefc58ab38d69947fcbbca60
Tyler K <ambiguousemails@proton.me>
2024-08-14 15:38:05 -0700

    Adding properties in .cpp

--- src/ser_comm.cpp
+++ src/ser_comm.cpp
@@ -11,7 +11,27 @@ void SerComm::_bind_methods()
 	ClassDB::bind_method(D_METHOD("close_serial"), &SerComm::sercomm_close);
 	ClassDB::bind_method(D_METHOD("read_serial"), &SerComm::sercomm_read);
 	ClassDB::bind_method(D_METHOD("write_serial", "p_message"), &SerComm::sercomm_write);
+
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "port"), "set_port", "get_port");
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "toggle_to_refresh"), "set_toggle_to_refresh", "get_toggle_to_refresh");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "baud_rate", PROPERTY_HINT_ENUM, VariantHelper::_baud_rate_to_hint_string()), "set_baud_rate", "get_baud_rate");
 	ADD_SIGNAL(MethodInfo("read_serial", PropertyInfo(Variant::STRING, "message")));
+}
+
+void SerComm::set_port(const int id) {
+	_port_enum = id; 
+}
+
+int SerComm::get_port() const {
+	return _port_enum;
+}
+
+void SerComm::set_baud_rate(const int b) {
+	baud_rate = b;
+}
+
+int SerComm::get_baud_rate() const {
+	return baud_rate;
 }
 
 SerComm::SerComm()
@@ -144,7 +164,7 @@ void SerComm::set_toggle_to_refresh(const bool p_is_toggled)
 	toggle_to_refresh = p_is_toggled;
 }
 
-bool SerComm::get_toggle_to_refresh()
+bool SerComm::get_toggle_to_refresh() const
 {
 	return toggle_to_refresh;
 }
@@ -184,17 +204,17 @@ bool SerComm::_get(const StringName &p_name, Variant &r_value) const
 {
 	if (p_name == StringName("port"))
 	{
-		r_value = _port_enum;
+		r_value = get_port();
 		return true;
 	}
 	else if (p_name == StringName("baud_rate"))
 	{
-		r_value = baud_rate;
+		r_value = get_baud_rate();
 		return true;
 	}
 	else if (p_name == StringName("toggle_to_refresh"))
 	{
-		r_value = toggle_to_refresh;
+		r_value = get_toggle_to_refresh();
 		return true;
 	}
 
@@ -205,17 +225,17 @@ bool SerComm::_set(const StringName &p_name, const Variant &p_value)
 {
 	if (p_name == StringName("port"))
 	{
-		_port_enum = p_value;
+		set_port(p_value);
 		return true;
 	}
 	else if (p_name == StringName("baud_rate"))
 	{
-		baud_rate = p_value;
+		set_baud_rate(p_value);
 		return true;
 	}
 	else if (p_name == StringName("toggle_to_refresh"))
 	{
-		toggle_to_refresh = p_value;
+		set_toggle_to_refresh(p_value);
 		return true;
 	}
 
